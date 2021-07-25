5a421fc1e50c70ddcb0d5516452d32677a816e20
SEMA <janchevsem@mail.ru>
2021-07-25 11:27:47 +0500

    контейнер для приложения, старт сессии

--- vendor/phpshop/core/App.php
+++ vendor/phpshop/core/App.php
@@ -6,8 +6,10 @@ namespace phpshop;
 
 class App
 {
+    public static $app;
+
     public function __construct(){
         $query = trim($_SERVER['QUERY_STRING'], '/');
-        var_dump($query);
+        session_start();
     }
 }

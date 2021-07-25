ea8a74712370465fc85df9071072f7dfc6399c2e
SEMA <janchevsem@mail.ru>
2021-07-25 16:54:17 +0500

    метод извлечения массива настроек

--- vendor/phpshop/core/App.php
+++ vendor/phpshop/core/App.php
@@ -11,6 +11,17 @@ class App
     public function __construct(){
         $query = trim($_SERVER['QUERY_STRING'], '/');
         session_start();
-        self::$app = Registry::$instance;
+        self::$app = Registry::instance();
+        $this->getParams();
+    }
+
+    protected function getParams()
+    {
+        $params = require_once CONF . '/params.php';
+        if (!empty($params)) {
+            foreach ($params as $k=>$v) {
+                self::$app->setProperty($k, $v);
+            }
+        }
     }
 }

d60b830c53449ebb805da28d0af0450155db941d
SEMA <janchevsem@mail.ru>
2021-07-27 21:17:59 +0500

    методы для добавления data и мета-данных

--- vendor/phpshop/core/base/Controller.php
+++ vendor/phpshop/core/base/Controller.php
@@ -22,7 +22,18 @@ abstract class Controller
         $this->view = $route['action'];
         $this->prefix = $route['prefix'];
 
-
     }
 
+    public function set($data)
+    {
+        $this->data = $data;
+    }
+
+    public function setMeta($title = '', $desc = '', $keywords = '')
+    {
+        $this->meta['title'] = $title;
+        $this->meta['desc'] = $desc;
+        $this->meta['keywords'] = $keywords;
+
+    }
 }

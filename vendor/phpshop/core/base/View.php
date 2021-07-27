05d43a3335bc23f16dfa758799a8cebdfc1a2a6e
SEMA <janchevsem@mail.ru>
2021-07-27 21:25:48 +0500

    свойства и конструктор View

--- vendor/phpshop/core/base/View.php
+++ vendor/phpshop/core/base/View.php
@@ -6,5 +6,23 @@ namespace phpshop\base;
 
 class View
 {
+    public $route;
+    public $controller;
+    public $view;
+    public $model;
+    public $prefix;
+    public $data = [];
+    public $meta = [];
 
+    public function __construct($route, $layout = '', $view = '', $meta)
+    {
+        $this->route = $route;
+        $this->controller = $route['controller'];
+        $this->model = $route['controller'];
+        $this->view = $view;
+        $this->prefix = $route['prefix'];
+        $this->meta = $meta;
+        $this->prefix = $route['prefix'];
+
+    }
 }

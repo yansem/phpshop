1834ccaf1172bdbb69cd0cc9bb6498ab81b46191
SEMA <janchevsem@mail.ru>
2021-07-28 18:41:56 +0500

    вызов метода setmeta

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -12,5 +12,6 @@ class MainController extends AppController
     public function indexAction()
     {
 //        echo __METHOD__;
+        $this->setMeta('Главная страница', 'Описание...', 'Ключевые слова...');
     }
 }

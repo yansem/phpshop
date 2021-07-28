fd3191967bc31411b8b460146f4af9d3f20a1885
SEMA <janchevsem@mail.ru>
2021-07-28 20:46:50 +0500

    get-параметры в маршруте

--- vendor/phpshop/core/Router.php
+++ vendor/phpshop/core/Router.php
@@ -26,6 +26,7 @@ class Router
 
     public static function dispatch($url)
     {
+        $url = self::removeQueryString($url);
         if (self::matchRoute($url)){
             $controller = 'app\controllers\\' . self::$route['prefix'] . self::$route['controller'] .
                 'Controller';
@@ -82,5 +83,17 @@ class Router
     protected static function lowerCamelCase($name)
     {
         return lcfirst(self::upperCamelCase($name));
+    }
+
+    protected static function removeQueryString($url)
+    {
+        if($url){
+            $params = explode('&', $url, 2);
+            if(strpos($params[0], '=') === false){
+                return rtrim($params[0], '/');
+            }else{
+                return '';
+            }
+        }
     }
 }

0de2a1fb5d7a142b537fcce81c3cfc9c1d68f51a
SEMA <janchevsem@mail.ru>
2021-07-31 09:42:20 +0500

    подключение библиотеки RedBeansPhp

--- public/index.php
+++ public/index.php
@@ -3,4 +3,5 @@
 require_once dirname(__DIR__) . '/config/init.php';
 require_once LIBS . '/functions.php';
 require_once CONF . '/routes.php';
+require_once ROOT . '/vendor/rb.php';
 new \phpshop\App();

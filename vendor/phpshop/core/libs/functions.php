ddc5a1e555b211b9977a0f360649d2366e58ea5e
SEMA <janchevsem@mail.ru>
2021-08-07 09:24:02 +0500

    функция редиректа

--- vendor/phpshop/core/libs/functions.php
+++ vendor/phpshop/core/libs/functions.php
@@ -4,3 +4,14 @@ function debug($array)
 {
     echo "<pre>" . print_r($array, true) . "</pre>";
 }
+
+function redirect($http=false)
+{
+    if($http){
+        $redirect = $http;
+    }else{
+        $redirect = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : PATH;
+    }
+    header("Location", $redirect);
+    exit;
+}

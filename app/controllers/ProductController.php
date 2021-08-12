d1081848eca55c184a99b6451f7e7e6f933dcb44
SEMA <janchevsem@mail.ru>
2021-08-12 23:06:13 +0500

    связанные товары

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -15,6 +15,8 @@ class ProductController extends AppController
         // хлебные крошки
 
         // связанные товары
+        $related = \R::getAll("SELECT * FROM related_product JOIN product ON 
+    product.id=related_product.related_id WHERE related_product.product_id=?", [$product->id]);
 
         // запись в куки просмотренного товаров
 
@@ -25,7 +27,7 @@ class ProductController extends AppController
         // модификации
 
         $this->setMeta($product->title, $product->description, $product->keywords);
-        $this->set(compact('product'));
+        $this->set(compact('product', 'related'));
     }
 
 }

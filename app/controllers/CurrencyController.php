056ccb04692b827bb99993bd64fa0cfd8f8c67d7
SEMA <janchevsem@mail.ru>
2021-09-05 10:27:37 +0500

    sql-запрос на выбранную валюту

--- app/controllers/CurrencyController.php
+++ app/controllers/CurrencyController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 
+use app\models\Cart;
 use phpshop\App;
 use phpshop\base\Controller;
 
@@ -12,6 +13,7 @@ class CurrencyController extends AppController
     {
         $currency = !empty($_GET['curr']) ? $_GET['curr'] : null;
         if($currency){
+            $curr = \R::findOne('currency', 'code = ?', [$currency]);
             if(array_key_exists($currency, App::$app->getProperty('currensies'))){
                 setcookie('currency', $currency, time()+3600*24*7, '/');
             }

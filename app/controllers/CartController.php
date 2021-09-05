aa037e18d37a9f27ab7ae5d115451875bc2b2713
SEMA <janchevsem@mail.ru>
2021-09-05 08:56:16 +0500

    корзина - очистка корзины

--- app/controllers/CartController.php
+++ app/controllers/CartController.php
@@ -12,18 +12,18 @@ class CartController extends AppController
         $qty = !empty($_GET['qty']) ? $_GET['qty'] : null;
         $mod_id = !empty($_GET['mod']) ? $_GET['mod'] : null;
         $mod = null;
-        if($id){
+        if ($id) {
             $product = \R::findOne('product', 'id=?', [$id]);
-            if(!$product){
+            if (!$product) {
                 return false;
             }
         }
-        if($mod_id){
+        if ($mod_id) {
             $mod = \R::findOne('modification', 'id=? AND product_id=?', [$mod_id, $id]);
         }
         $cart = new Cart();
         $cart->addToCart($product, $qty, $mod);
-        if($this->isAjax()){
+        if ($this->isAjax()) {
             $this->loadView('cart_modal');
         }
         redirect();
@@ -37,14 +37,23 @@ class CartController extends AppController
     public function deleteAction()
     {
         $id = !empty($_GET['id']) ? $_GET['id'] : null;
-        if(isset($_SESSION['cart'][$id])){
+        if (isset($_SESSION['cart'][$id])) {
             $cart = new Cart();
             $cart->deleteItem($id);
         }
 
-        if($this->isAjax()){
+        if ($this->isAjax()) {
             $this->loadView('cart_modal');
         }
         redirect();
+    }
+
+    public function clearAction()
+    {
+        unset($_SESSION['cart']);
+        unset($_SESSION['cart.qty']);
+        unset($_SESSION['cart.sum']);
+        unset($_SESSION['cart.currency']);
+        $this->loadView('cart_modal');
     }
 }

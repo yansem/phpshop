0dd6645932e8ae435e92327064c05891fb4bf8eb
SEMA <janchevsem@mail.ru>
2021-09-06 08:47:13 +0500

    поиск typeahead bloodhound

--- app/views/layouts/watches.php
+++ app/views/layouts/watches.php
@@ -94,9 +94,13 @@ License URL: http://creativecommons.org/licenses/by/3.0/
             </div>
             <div class="col-md-3 header-right">
                 <div class="search-bar">
-                    <input type="text" value="Search" onfocus="this.value = '';"
-                           onblur="if (this.value == '') {this.value = 'Search';}">
-                    <input type="submit" value="">
+                    <form action="search" method="get" autocomplete="off">
+                        <input type="text" class="typeahead" id="typeahead" name="s">
+                        <input type="submit" value="">
+                    </form>
+<!--                    <input type="text" value="Search" onfocus="this.value = '';"-->
+<!--                           onblur="if (this.value == '') {this.value = 'Search';}">-->
+<!--                    <input type="submit" value="">-->
                 </div>
             </div>
             <div class="clearfix"></div>
@@ -205,6 +209,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
 <script src="js/jquery-1.11.0.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="js/typeahead.bundle.js"></script>
 <!--dropdown-->
 <script src="js/jquery.easydropdown.js"></script>
 <script src="js/responsiveslides.min.js"></script>

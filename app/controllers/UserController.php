1231088ec9b6cefcc6784cf26a3a998315736d2e
SEMA <janchevsem@mail.ru>
2021-09-07 19:17:51 +0500

    регистрация - сохранение введенных данных при неудаче

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -21,6 +21,7 @@ class UserController extends AppController
                 }
             }else{
                 $user->getErrors();
+                $_SESSION['form_data'] = $data;
             }
             redirect();
         }
--- app/views/User/signup.php
+++ app/views/User/signup.php
@@ -25,35 +25,36 @@
                             <form method="post" action="user/signup" id="signup" role="form" data-toggle="validator">
                                 <div class="form-group has-feedback">
                                     <label for="login" class="control-label">Login</label>
-                                    <input type="text" name="login" class="form-control" id="login" placeholder="Login" required>
+                                    <input type="text" name="login" class="form-control" id="login" placeholder="Login" value="<?= isset($_SESSION['form_data']['login']) ? h($_SESSION['form_data']['login']) : ''; ?>" required>
                                     <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                                 </div>
 
                                 <div class="form-group has-feedback">
                                     <label for="pasword" class="control-label">Password</label>
-                                    <input type="password" data-minlength="6" name="password" class="form-control" id="pasword" placeholder="Password" required>
+                                    <input type="password" data-minlength="6" name="password" class="form-control" id="pasword" placeholder="Password" value="<?= isset($_SESSION['form_data']['password']) ? h($_SESSION['form_data']['password']) : ''; ?>" required>
                                     <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                                     <div class="help-block">Пароль должен включать не менее 6 символов</div>
                                 </div>
 
                                 <div class="form-group has-feedback">
                                     <label for="name" class="control-label">Имя</label>
-                                    <input type="text" name="name" class="form-control" id="name" placeholder="Имя" required>
+                                    <input type="text" name="name" class="form-control" id="name" placeholder="Имя" value="<?= isset($_SESSION['form_data']['name']) ? h($_SESSION['form_data']['name']) : ''; ?>" required>
                                     <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                                 </div>
                                 <div class="form-group has-feedback">
                                     <label for="email" class="control-label">Email</label>
-                                    <input type="email" name="email" class="form-control" id="email" placeholder="Email" data-error="Bruh, that email address is invalid" required>
+                                    <input type="email" name="email" class="form-control" id="email" placeholder="Email" data-error="Bruh, that email address is invalid" value="<?= isset($_SESSION['form_data']['email']) ? h($_SESSION['form_data']['email']) : ''; ?>" required>
                                     <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                                     <div class="help-block with-errors"></div>
                                 </div>
                                 <div class="form-group has-feedback">
                                     <label for="address" class="control-label">Address</label>
-                                    <input type="text" name="address" class="form-control" id="address" placeholder="Address" required>
+                                    <input type="text" name="address" class="form-control" id="address" placeholder="Address" value="<?= isset($_SESSION['form_data']['address']) ? h($_SESSION['form_data']['address']) : ''; ?>" required>
                                     <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
                                 </div>
                                 <button type="submit" class="btn btn-default">Зарегистрировать</button>
                             </form>
+                            <?php if(isset($_SESSION['form_data'])) unset($_SESSION['form_data']); ?>
                         </div>
                     </div>
                 </div>

a425ab7f0d4ff29795684c98f7f32c429574f463
SEMA <janchevsem@mail.ru>
2021-09-08 10:13:44 +0500

    оформление заказа - модель заказа

---
+++ app/models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\models;
+
+class Order
+{
+    public static function saveOrder($data)
+    {
+
+    }
+
+    public static function saveOrderProduct($order_id)
+    {
+
+    }
+
+    public static function mailOrder($order_id, $user_email)
+    {
+
+    }
+}

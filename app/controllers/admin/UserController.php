f45f55f8f552b432e18b6ad4969ff1ec0653d75a
SEMA <janchevsem@mail.ru>
2021-09-12 21:31:00 +0500

    авторизация админа - форма, регистрация, вывод запросов, чистка шаблона

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -2,10 +2,25 @@
 
 namespace app\controllers\admin;
 
+use app\models\User;
+
 class UserController extends AppController
 {
     public function loginAdminAction()
     {
-        die('111');
+        $this->layout = 'login';
+        if(!empty($_POST)){
+            $user = new User();
+            if($user->login(true)){
+                $_SESSION['success'] = 'Вы успешно авторизованы';
+            }else{
+                $_SESSION['error'] = 'Логин/пароль введены неверно';
+            }
+            if(User::isAdmin()){
+                redirect(ADMIN);
+            }else{
+                redirect();
+            }
+        }
     }
 }

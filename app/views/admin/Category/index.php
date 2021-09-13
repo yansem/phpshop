f1fc864821bfa1b325ce7b6d6f8ea2766088bb1f
SEMA <janchevsem@mail.ru>
2021-09-13 20:18:39 +0500

    правка

--- app/views/admin/Category/index.php
+++ app/views/admin/Category/index.php
@@ -5,7 +5,7 @@
     </h1>
     <ol class="breadcrumb">
         <li><a href="<?=ADMIN;?>"><i class="fa fa-dashboard"></i> Главная</a></li>
-        <li class="active"><i class="fa fa-dashboard"></i> Список заказов</li>
+        <li class="active"><i class="fa fa-dashboard"></i> Список категорий</li>
     </ol>
 </section>
 

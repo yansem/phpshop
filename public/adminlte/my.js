1059edc32cfba7f8835d155b08669dbf94ebcff5
SEMA <janchevsem@mail.ru>
2021-09-13 19:11:13 +0500

    админ-список категорий, активная ссылка

--- public/adminlte/my.js
+++ public/adminlte/my.js
@@ -2,3 +2,11 @@ $('.delete').click(function (){
     let res = confirm('Подтвердите действие');
     if(!res) return false;
 })
+
+let loc = window.location.protocol + '//' + window.location.host + window.location.pathname;
+$('.sidebar-menu a').each(function (){
+    if($(this).attr('href') == loc){
+        $(this).parent().addClass('active');
+        $(this).closest('.treeview').addClass('active');
+    }
+})

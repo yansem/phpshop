9031281989c5cab353ee6586fb785913c978aaf3
SEMA <janchevsem@mail.ru>
2021-09-14 13:03:04 +0500

    список пользователей

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -3,9 +3,25 @@
 namespace app\controllers\admin;
 
 use app\models\User;
+use phpshop\libs\Pagination;
 
 class UserController extends AppController
 {
+    public function indexAction()
+    {
+        $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+        $perpage = 3;
+        $count = \R::count('user');
+        $pagination = new Pagination($page, $perpage, $count);
+        $start = $pagination->getStart();
+
+        $users = \R::findAll('user',"LIMIT $start, $perpage");
+        $this->set(compact('users', 'pagination', 'count'));
+        $this->setMeta('Список пользователей');
+    }
+
+
+
     public function loginAdminAction()
     {
         $this->layout = 'login';

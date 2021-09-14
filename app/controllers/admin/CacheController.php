600c3adcad1ffa4c4a944337ca616a16df5496ab
SEMA <janchevsem@mail.ru>
2021-09-14 11:16:09 +0500

    админ-очистка кэша

---
+++ app/controllers/admin/CacheController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\controllers\admin;
+
+use phpshop\Cache;
+
+class CacheController extends AppController
+{
+    public function indexAction()
+    {
+        $this->setMeta('Очистка кэша');
+    }
+
+    public function deleteAction()
+    {
+        $key = isset($_GET['key']) ? $_GET['key'] : null;
+        $cache = Cache::instance();
+        switch ($key){
+            case 'category':
+                $cache->delete('cats');
+                $cache->delete('phpshop_menu');
+                break;
+
+            case 'filter':
+                $cache->delete('filter_group');
+                $cache->delete('filter_attrs');
+                break;
+        }
+        $_SESSION['success'] = 'Выбранный кэш удален';
+        redirect();
+    }
+}

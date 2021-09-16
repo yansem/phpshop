5c9a08f520a0b70bc4651e9a6255dbf6fc371454
SEMA <janchevsem@mail.ru>
2021-09-16 09:20:36 +0500

    условие создания AjaxUpload

--- public/adminlte/my.js
+++ public/adminlte/my.js
@@ -37,53 +37,60 @@ $(".select2").select2({
             return {
                 results: data.items,
             };
+        }
+    }
+});
+
+if($('div').is('#single')){
+    var buttonSingle = $("#single"),
+        buttonMulti = $("#multi"),
+        file;
+}
+
+if(buttonSingle){
+    new AjaxUpload(buttonSingle, {
+        action: adminpath + buttonSingle.data('url') + "?upload=1",
+        data: {name: buttonSingle.data('name')},
+        name: buttonSingle.data('name'),
+        onSubmit: function(file, ext){
+            if (! (ext && /^(jpg|png|jpeg|gif)$/i.test(ext))){
+                alert('Ошибка! Разрешены только картинки');
+                return false;
+            }
+            buttonSingle.closest('.file-upload').find('.overlay').css({'display':'block'});
+
         },
-    },
-});
+        onComplete: function(file, response){
+            setTimeout(function(){
+                buttonSingle.closest('.file-upload').find('.overlay').css({'display':'none'});
 
-var buttonSingle = $("#single"),
-    buttonMulti = $("#multi"),
-    file;
-new AjaxUpload(buttonSingle, {
-    action: adminpath + buttonSingle.data('url') + "?upload=1",
-    data: {name: buttonSingle.data('name')},
-    name: buttonSingle.data('name'),
-    onSubmit: function(file, ext){
-        if (! (ext && /^(jpg|png|jpeg|gif)$/i.test(ext))){
-            alert('Ошибка! Разрешены только картинки');
-            return false;
+                response = JSON.parse(response);
+                $('.' + buttonSingle.data('name')).html('<img src="/images/' + response.file + '" style="max-height: 150px;">');
+            }, 1000);
         }
-        buttonSingle.closest('.file-upload').find('.overlay').css({'display':'block'});
+    });
+}
 
-    },
-    onComplete: function(file, response){
-        setTimeout(function(){
-            buttonSingle.closest('.file-upload').find('.overlay').css({'display':'none'});
+if(buttonMulti){
+    new AjaxUpload(buttonMulti, {
+        action: adminpath + buttonMulti.data('url') + "?upload=1",
+        data: {name: buttonMulti.data('name')},
+        name: buttonMulti.data('name'),
+        onSubmit: function(file, ext){
+            if (! (ext && /^(jpg|png|jpeg|gif)$/i.test(ext))){
+                alert('Ошибка! Разрешены только картинки');
+                return false;
+            }
+            buttonMulti.closest('.file-upload').find('.overlay').css({'display':'block'});
 
-            response = JSON.parse(response);
-            $('.' + buttonSingle.data('name')).html('<img src="/images/' + response.file + '" style="max-height: 150px;">');
-        }, 1000);
-    }
-});
+        },
+        onComplete: function(file, response){
+            setTimeout(function(){
+                buttonMulti.closest('.file-upload').find('.overlay').css({'display':'none'});
 
-new AjaxUpload(buttonMulti, {
-    action: adminpath + buttonMulti.data('url') + "?upload=1",
-    data: {name: buttonMulti.data('name')},
-    name: buttonMulti.data('name'),
-    onSubmit: function(file, ext){
-        if (! (ext && /^(jpg|png|jpeg|gif)$/i.test(ext))){
-            alert('Ошибка! Разрешены только картинки');
-            return false;
+                response = JSON.parse(response);
+                $('.' + buttonMulti.data('name')).append('<img src="/images/' + response.file + '" style="max-height: 150px;">');
+            }, 1000);
         }
-        buttonMulti.closest('.file-upload').find('.overlay').css({'display':'block'});
-
-    },
-    onComplete: function(file, response){
-        setTimeout(function(){
-            buttonMulti.closest('.file-upload').find('.overlay').css({'display':'none'});
-
-            response = JSON.parse(response);
-            $('.' + buttonMulti.data('name')).append('<img src="/images/' + response.file + '" style="max-height: 150px;">');
-        }, 1000);
-    }
-});
+    });
+}

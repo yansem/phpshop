95189de850a7f596dc0b84f54b70af9703dad8ff
SEMA <janchevsem@mail.ru>
2021-09-17 14:25:36 +0500

    админ-редактирование валют

--- app/controllers/admin/CurrencyController.php
+++ app/controllers/admin/CurrencyController.php
@@ -13,6 +13,29 @@ class CurrencyController extends AppController
         $this->set(compact('currencies'));
     }
 
+    public function editAction(){
+        if(!empty($_POST)){
+            $id = $this->getRequestID(false);
+            $currency = new Currency();
+            $data = $_POST;
+            $currency->load($data);
+            $currency->attributes['base'] = $currency->attributes['base'] ? '1' : '0';
+            if(!$currency->validate($data)){
+                $currency->getErrors();
+                redirect();
+            }
+            if($currency->update('currency', $id)){
+                $_SESSION['success'] = "Изменения сохранены";
+                redirect();
+            }
+        }
+
+        $id = $this->getRequestID();
+        $currency = \R::load('currency', $id);
+        $this->setMeta("Редактирование валюты {$currency->title}");
+        $this->set(compact('currency'));
+    }
+
     public function addAction()
     {
         if(!empty($_POST)){

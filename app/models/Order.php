6dc3a7f761e4a71f200916f5184f78695169de40
SEMA <janchevsem@mail.ru>
2021-09-21 13:07:05 +0500

    почта-try catch

--- app/models/Order.php
+++ app/models/Order.php
@@ -37,24 +37,34 @@ class Order extends AppModel
 
     public static function mailOrder($order_id, $user_email)
     {
-        $transport = (new Swift_SmtpTransport(App::$app->getProperty('smtp_host'), App::$app->getProperty('smtp_port'), App::$app->getProperty('smtp_protocol')))
-            ->setUsername(App::$app->getProperty('smtp_login'))
-            ->setPassword(App::$app->getProperty('smtp_password'));
+        try {
+            $transport = (new Swift_SmtpTransport(App::$app->getProperty('smtp_host'), App::$app->getProperty('smtp_port'), App::$app->getProperty('smtp_protocol')))
+                ->setUsername(App::$app->getProperty('smtp_login'))
+                ->setPassword(App::$app->getProperty('smtp_password'));
 
-        $mailer = new Swift_Mailer($transport);
+            $mailer = new Swift_Mailer($transport);
 
-        ob_start();
-        require_once APP . '/views/mail/mail_order.php';
-        $body = ob_get_clean();
+            ob_start();
+            require_once APP . '/views/mail/mail_order.php';
+            $body = ob_get_clean();
 
-        $message = (new Swift_Message("Заказ №{$order_id}"))
-            ->setFrom([App::$app->getProperty('smtp_login') => App::$app->getProperty('shop_name')])
-            ->setTo($user_email)
-            ->setBody($body, 'text/html')
-        ;
+            $message_client = (new Swift_Message("Вы совершили заказ №{$order_id} на сайте " . App::$app->getProperty('shop_name')))
+                ->setFrom([App::$app->getProperty('smtp_login') => App::$app->getProperty('shop_name')])
+                ->setTo($user_email)
+                ->setBody($body, 'text/html')
+            ;
+
+            $message_admin = (new Swift_Message("Сделан заказ №{$order_id}"))
+                ->setFrom([App::$app->getProperty('smtp_login') => App::$app->getProperty('shop_name')])
+                ->setTo(App::$app->getProperty('admin_email'))
+                ->setBody($body, 'text/html')
+            ;
 
 // Send the message
-        $result = $mailer->send($message);
+            $result = $mailer->send($message_client);
+            $result = $mailer->send($message_admin);
+        }catch(\Exception $e){
+        }
         unset($_SESSION['cart']);
         unset($_SESSION['cart.qty']);
         unset($_SESSION['cart.sum']);
